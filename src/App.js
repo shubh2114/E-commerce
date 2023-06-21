e8dfa834bcd89010132048c61834c2db2a3f8b8b
shubham <shubh2114@gmail.com>
2023-06-21 23:43:29 +0530

    added the routes and the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
 
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.component";
+import {  Routes, Route , Outlet} from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
+import Signin from "./routes/sign-in/Signin.component";
+
+const Shop = ()=> {
+  return (
+    <h1>hello its shop</h1>
+  )
+}
+
+
+
 
 const App = ()=> {
-
-  const categories=[
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
+  
 
   return (
-    
-    <Directory categories={categories}/>
-   
-  );
+  <Routes>
+   <Route path="/" element={<Navigation />} >
+  <Route index element={<Home/>}  />
+  <Route path='shop' element={<Shop/>}  />
+  <Route path='sign-in' element={<Signin/>}  />
+  </Route> 
+  </Routes>
+
+
+  )
+
+  
+
 }
 
 export default App;
